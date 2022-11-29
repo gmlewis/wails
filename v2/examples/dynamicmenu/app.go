62f7c70e934c996705f33031f5a9cbad6da94fb4
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-11-28 21:40:27 -0500

    Minimal example
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- v2/examples/dynamicmenu/app.go
+++ v2/examples/dynamicmenu/app.go
@@ -3,16 +3,26 @@ package main
 import (
 	"context"
 	"fmt"
+
+	"github.com/wailsapp/wails/v2/pkg/menu"
 )
 
 // App struct
 type App struct {
 	ctx context.Context
+
+	language string
+
+	appMenu  *menu.Menu
+	fileMenu *menu.Menu
+	langMenu *menu.Menu
 }
 
 // NewApp creates a new App application struct
 func NewApp() *App {
-	return &App{}
+	return &App{
+		language: "A",
+	}
 }
 
 // startup is called when the app starts. The context is saved
--- v2/examples/dynamicmenu/main.go
+++ v2/examples/dynamicmenu/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"embed"
+	"fmt"
 
 	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/menu"
+	"github.com/wailsapp/wails/v2/pkg/menu/keys"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 //go:embed all:frontend/dist
@@ -14,12 +18,14 @@ var assets embed.FS
 func main() {
 	// Create an instance of the app structure
 	app := NewApp()
+	app.makeMenu()
 
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "dynamicmenu",
 		Width:  1024,
 		Height: 768,
+		Menu:   app.appMenu,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
@@ -33,4 +39,34 @@ func main() {
 	if err != nil {
 		println("Error:", err.Error())
 	}
+}
+
+func (a *App) makeMenu() {
+	a.appMenu = menu.NewMenu()
+
+	a.fileMenu = a.appMenu.AddSubmenu("File - A")
+
+	a.langMenu = a.fileMenu.AddSubmenu("Language - A")
+	a.langMenu.AddRadio("A", true, nil, a.changeLanguage)
+	a.langMenu.AddRadio("B", false, nil, a.changeLanguage)
+	a.langMenu.AddRadio("C", false, nil, a.changeLanguage)
+
+	a.fileMenu.AddSeparator()
+	a.fileMenu.AddText("Quit - A", keys.CmdOrCtrl("q"), func(_ *menu.CallbackData) {
+		runtime.Quit(a.ctx)
+	})
+}
+
+func (a *App) changeLanguage(data *menu.CallbackData) {
+	if a.language == data.MenuItem.Label {
+		return
+	}
+	a.language = data.MenuItem.Label
+
+	a.appMenu.Items[0].Label = fmt.Sprintf("File - %v", a.language)
+	a.fileMenu.Items[0].Label = fmt.Sprintf("Language - %v", a.language)
+	a.fileMenu.Items[2].Label = fmt.Sprintf("Quit - %v", a.language)
+
+	runtime.MenuSetApplicationMenu(a.ctx, a.appMenu) // crashes on Linux
+	// runtime.MenuUpdateApplicationMenu(a.ctx) // does nothing on Linux
 }
